0d25c7951b974a2143c3464604cd8c7d4057449e
wbenner12 <146144834+wbenner12@users.noreply.github.com>
2023-10-23 23:39:04 +0000

    coim

--- app.rb
+++ app.rb
@@ -5,6 +5,23 @@ get("/howdy") do
   erb(:hello)
 end
 
+get("/goodbye") do
+  erb(:bye)
+end
+
+get("/square/new") do
+  erb(:new_square_calc)
+end
+
+get ("/square/results") do
+  @the_num = params.fetch("users_number").to_f
+
+  @the_result = @the_num ** 2
+  
+  erb(:square_results)
+end
+
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
